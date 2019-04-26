eb7813dea21a37890c6a9d14eb026da9961dc257
HouariZEGAI <houarizegai14@gmail.com>
2019-04-26 14:33:21 +0100

    Making welcome page ui & init css files

---
+++ src/com/houarizegai/messengerfx/java/controllers/WelcomeController.java
@@ -0,0 +1,11 @@
+package com.houarizegai.messengerfx.java.controllers;
+
+import javafx.fxml.FXML;
+
+public class WelcomeController {
+
+    @FXML
+    private void onContinueAs() {
+        System.out.println("Hello Houari ZEGAI!");
+    }
+}
