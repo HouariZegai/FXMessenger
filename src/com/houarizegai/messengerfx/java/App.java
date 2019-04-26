f476569065f49c8368653d11ae851f9cbd8f0f15
HouariZEGAI <houarizegai14@gmail.com>
2019-04-26 14:34:40 +0100

    Making icon for app & first app run!

---
+++ src/com/houarizegai/messengerfx/java/App.java
@@ -0,0 +1,32 @@
+package com.houarizegai.messengerfx.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            // get main views (fxml file)
+            Parent root = FXMLLoader.load(getClass().getResource("/com/houarizegai/messengerfx/resources/views/Welcome.fxml"));
+            // make icon for app
+            stage.getIcons().add(new Image("/com/houarizegai/messengerfx/resources/images/icons/icon_app.png"));
+            stage.setScene(new Scene(root));
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+        stage.setTitle("MessengerFX");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
