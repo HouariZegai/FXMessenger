854201b00699f557e2c5884bdaa2df939e16aaaf
HouariZEGAI <houarizegai14@gmail.com>
2019-04-26 16:18:58 +0100

    Making forgot password front-end (ui)

---
+++ src/com/houarizegai/messengerfx/java/controllers/ForgotPasswordController.java
@@ -0,0 +1,20 @@
+package com.houarizegai.messengerfx.java.controllers;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class ForgotPasswordController {
+
+    @FXML
+    private JFXTextField fieldEmailPhone;
+
+    @FXML
+    private void onContinue() {
+
+    }
+
+    @FXML
+    private void onBack() {
+
+    }
+}
