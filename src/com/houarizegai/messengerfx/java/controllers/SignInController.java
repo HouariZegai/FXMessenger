4f5e64e28970894c05166c85ea89862b2300815a
HouariZEGAI <houarizegai14@gmail.com>
2019-04-26 16:44:03 +0100

    Making sign in form - front-end (ui)

---
+++ src/com/houarizegai/messengerfx/java/controllers/SignInController.java
@@ -0,0 +1,34 @@
+package com.houarizegai.messengerfx.java.controllers;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class SignInController {
+
+    @FXML
+    private JFXTextField fieldEmailPhone;
+
+    @FXML
+    private JFXPasswordField fieldPassword;
+
+    @FXML
+    private void onSignIn() {
+
+    }
+
+    @FXML
+    private void onForgotPassword() {
+
+    }
+
+    @FXML
+    private void onNotOnFacebook() {
+
+    }
+
+    @FXML
+    private void onBack() {
+
+    }
+}
