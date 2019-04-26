4d054da9f9e59f761abd7a3db4e1816e454d7237
HouariZEGAI <houarizegai14@gmail.com>
2019-04-26 17:20:32 +0100

    Making sign up using phone number - front-end

---
+++ src/com/houarizegai/messengerfx/java/controllers/SignUpController.java
@@ -0,0 +1,30 @@
+package com.houarizegai.messengerfx.java.controllers;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class SignUpController {
+
+    @FXML
+    private JFXTextField fieldPhone;
+
+    @FXML
+    private void onContinue() {
+
+    }
+
+    @FXML
+    private void onPrivacyPolicy() {
+
+    }
+
+    @FXML
+    private void onTerms() {
+
+    }
+
+    @FXML
+    private void onBack() {
+
+    }
+}
