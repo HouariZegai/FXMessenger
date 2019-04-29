5ff56703b4f5fc930686c381309ffcd6412a0212
HouariZEGAI <houarizegai14@gmail.com>
2019-04-29 10:58:34 +0100

    Adding call UI (front-end)

---
+++ src/com/houarizegai/messengerfx/java/controllers/CallController.java
@@ -0,0 +1,23 @@
+package com.houarizegai.messengerfx.java.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class CallController {
+    @FXML
+    private ImageView imgProfileCall;
+
+    @FXML
+    private Label fieldCallName;
+
+    @FXML
+    private void onFinishCall() {
+
+    }
+
+    @FXML
+    private void onBack() {
+
+    }
+}
